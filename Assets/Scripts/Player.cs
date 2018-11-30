a6eaf52aacb9988e58190392ade00200c967703c
Trooperjack <43067312+Trooperjack@users.noreply.github.com>
2018-11-30 15:42:43 +0000

    Added Free Assets, Created Basic Top-Down Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Extra using statement to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
+
+public class Player : MonoBehaviour {
+
+    // designer variables
+    public float speed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float x = Input.GetAxis(horizontalAxis);
+        float y = Input.GetAxis(verticalAxis);
+        physicsBody.velocity = new Vector2(x * speed, y * speed);
+
+    }
+}
