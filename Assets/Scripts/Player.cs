1f12917dcc64c8482d69067e15f548acfb950486
Trooperjack <43067312+Trooperjack@users.noreply.github.com>
2018-12-04 16:40:11 +0000

    Added basic Weapon Shooting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,25 +13,96 @@ public class Player : MonoBehaviour {
     public Rigidbody2D physicsBody;
     public string horizontalAxis = "Horizontal";
     public string verticalAxis = "Vertical";
+    public string fireAxis = "Fire2";
+    public string playerControl = "xy";
+    public bool enableWeapon;
+
+    public float lives = 3;
+    public float health = 100;
+    public float magazine = 0;
+    public float maxMagazine = 30;
+    public float ammo = 0;
+    public float maxAmmo = 120;
+    public float bulletForce = 1000;
 
     public Animator playerAnimator;
     public SpriteRenderer playerSprite;
     public Collider2D playerCollider;
+    
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
 
     // Use this for initialization
     void Start () {
-		
+
+        magazine = maxMagazine;
+        ammo = maxAmmo;
+        enableWeapon = true;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        float x = Input.GetAxis(horizontalAxis);
-        float y = Input.GetAxis(verticalAxis);
-        physicsBody.velocity = new Vector2(x * speed, y * speed);
+
+        //Current player movement axis
+        if (playerControl == "xy")
+        {
+            float x = Input.GetAxis(horizontalAxis);
+            float y = Input.GetAxis(verticalAxis);
+            physicsBody.velocity = new Vector2(x * speed, y * speed);
+        }
+        else if (playerControl == "x")
+        {
+            float x = Input.GetAxis(horizontalAxis);
+            physicsBody.velocity = new Vector2(x * speed, 0);
+        }
+        else if (playerControl == "y")
+        {
+            float y = Input.GetAxis(verticalAxis);
+            physicsBody.velocity = new Vector2(0, y * speed);
+        }
 
 
+        //When Fire button pressed
+        if (Input.GetButton("Fire1"))
+        {
+            if (magazine > 0 && enableWeapon == true)
+            {
+                FireWeapon();
+            }
+        }
+        
+
+        //When reload button pressed
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (magazine < 30)
+            {
+                magazine = 30;
+                ammo = ammo - 30;
+            }
+        }
 
 
     }
+
+
+
+    //Weapon fired
+    void FireWeapon()
+    {
+        magazine = magazine - 1;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (Vector2)((mousePos - transform.position));
+        direction.Normalize();
+
+        var Bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position + (Vector3)(direction * 0.5f), Quaternion.identity);
+        Bullet.GetComponent<Rigidbody2D>().AddForce(direction * bulletForce);
+        Destroy(Bullet, 2.0f);
+    }
+
+
+
 }
